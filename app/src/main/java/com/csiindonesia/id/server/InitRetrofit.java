8dc9a141e6399bfe801fc4a22c6f1e5fef17c3a8
Aseptrisna <Aseptrisna1995@gmail.com>
2022-08-15 23:56:08 +0700

    add new ui absent

--- app/src/main/java/com/csiindonesia/id/server/InitRetrofit.java
+++ app/src/main/java/com/csiindonesia/id/server/InitRetrofit.java
@@ -3,8 +3,8 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class InitRetrofit {
-//    public static final String BASE_URL ="https://absent-kita.pptik.id:5114/";
-    public static final String BASE_URL ="http://192.168.0.8:5114/";
+    public static final String BASE_URL ="https://absent-kita.pptik.id:5114/";
+//    public static final String BASE_URL ="http://192.168.0.8:5114/";
     private static InitRetrofit mInstance;
     private Retrofit retrofit;
     private InitRetrofit(){
